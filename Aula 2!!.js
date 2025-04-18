b9f886db48a1c6cdbdfb51c33a4df0af926f124f
PEDRO GABRIEL CALEIROS SILVA <pedro-gabriel_silva@estudante.sesisenai.org.br>
2025-04-17 21:31:20 -0300

    add calculadora de botão

---
+++ Aula 2!!.js
@@ -0,0 +1,25 @@
+function somar(){
+    let n1 , n2, soma
+  
+  
+    n1 = Number(prompt("Primeiro Número: "))
+    n2 = Number (prompt("Segundo Número: "))
+  
+    soma = n1 + n2 
+  
+    alert("Soma " + soma)
+      
+  }
+  
+  
+  
+  function Diminuir(){
+  
+    let n1, n2, diferenca 
+     n1 = Number(prompt("Primeiro Número: "))
+     n2 = Number(prompt("Segndo Número: "))
+  
+     diferenca = n1 - n2
+  
+    alert("Diferença: " + diferenca)
+}
