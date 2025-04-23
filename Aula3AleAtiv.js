89fb915606d7d2ff3010236deaeb70d0b1851aa3
PEDRO GABRIEL CALEIROS SILVA <pedro-gabriel_silva@estudante.sesisenai.org.br>
2025-04-22 21:58:50 -0300

    atividade nova do Alex e conteudos de aula

---
+++ Aula3AleAtiv.js
@@ -0,0 +1,14 @@
+function exercicio1(){
+   let numero1 = Number(prompt("Digite um Número"))
+   let numero2 = Number(prompt("Digite outro Número"))
+   let soma = numero1 + numero2
+
+   alert("A Soma dos Números é: " + soma)
+}
+function exercicio2(){
+    let numero1 = Number(prompt("Digite um Número"))
+    let numero2 = Number(prompt("Digite outro Número"))
+    let media = (numero1 + numero2)/2
+
+    alert("A Média dos dois Números é igual a: " + media)
+}
---
+++ ativ1Ale.js
@@ -0,0 +1,14 @@
+// 1. Exercício 1 - Saudação Personalizada
+function Saudação(){
+    let name = prompt("Digite seu nome")
+
+    alert("Óla, "+ name+"!"+"Seja Bem Vindo")
+}
+// 2. Exercício 2 - Calculadora de Soma
+function Calculadora(){
+    let numero1 = Number(prompt("Digite um Número"))
+    let numero2 = Number(prompt("Digite outro Número"))
+    let soma = numero1 + numero2
+ 
+    alert("A Soma dos Números é: " + soma)
+ }
